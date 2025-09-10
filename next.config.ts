81472d16b3ca67416f4d5fb8136c6322a0611f3d
Shreyansh Gajjar Mac <shreyanshgajjar2005@gmail.com>
2025-09-10 17:36:43 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  eslint: {
+    // Allow build to succeed even if there are ESLint warnings/errors
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
